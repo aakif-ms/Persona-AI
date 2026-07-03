dbacc4209d1b2675dc67c5375832d6b12478e2ab
aakif-ms <aakif.msiddiqui@gmail.com>
2026-07-03 23:53:41 +0530

    fix scope issue

--- backend/connectors/calendar.py
+++ backend/connectors/calendar.py
@@ -5,7 +5,10 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/calendar.readonly'
+]
 
 def get_calendar_service():
     creds = None
